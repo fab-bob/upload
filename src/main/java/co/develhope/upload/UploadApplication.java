a2e408d1957a507e77ca72bc021e5bf5d9b27c1b
Fabrizio Lagioia <meandi958@gmail.com>
2024-06-30 19:57:29 +0200

    upload exercise

---
+++ src/main/java/co/develhope/upload/UploadApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadApplication.class, args);
+	}
+
+}
